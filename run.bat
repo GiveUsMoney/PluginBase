6a6771d77d8b702b2f6bc7fc2187fe2d65d5192b
Roharui <jack555586@gmail.com>
2021-01-02 14:55:59 +0900

    MC Version change 1.16.3 -> 1.16.4

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 set oriPath=%~dp0
 cd ..\Server\
 cls
-java -Xms1024M -Xmx1024M -jar spigot-1.16.3.jar -o true nogui
+java -Xms1024M -Xmx1024M -jar spigot-1.16.4.jar -o true nogui
 PAUSE
 cd %oriPath%
 cls
